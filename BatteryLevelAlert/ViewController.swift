0de97d98c5f977c7c3cd5a8f9ecd9104c528fbea
dunkthelunk <prudhvi_porandla@icloud.com>
2017-11-20 23:35:22 +0530

    Initial Commit

---
+++ BatteryLevelAlert/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BatteryLevelAlert
+//
+//  Created by prudhvi on 20/11/17.
+//  Copyright © 2017 dunkthelunk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
